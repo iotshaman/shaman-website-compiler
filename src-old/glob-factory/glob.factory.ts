a724c4fc9c937a9f79bcb3e2fadb2b0928365558
Kyle Brown <kbrown.watco@gmail.com>
2018-04-22 12:10:53 -0500

    cleanup for 2.0

--- src-old/glob-factory/glob.factory.ts
+++
@@ -1,8 +0,0 @@
-import * as FastGlob from 'fast-glob';
-import * as Promise from 'promise';
-
-export function GlobFactory(options): (patterns: string[]) => Promise<any> {
-    return (patterns: string[]) => {
-        return new Promise((res) => { return FastGlob(patterns, options).then(res); });
-    }
-}
